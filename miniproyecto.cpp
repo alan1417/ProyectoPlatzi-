126204371ed026787594cd608b2f3bac4f82ecb4
alan1417 <112138010+alan1417@users.noreply.github.com>
2023-04-29 18:47:18 -0400

    Add files via upload

---
+++ miniproyecto.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;  
+int main() {
+    char nombre;
+    cout << "Hola! ¿Cual es tu nombre? ";
+    cin >> nombre;
+    cout << "¡Hola, " << nombre << "! Bienvenido al proyecto de Alan para certificarse en Platzi." << endl;
+    return 0;
+}
